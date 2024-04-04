3a9f2d0f90ca3ae9fd8ac9bfbd658ba7c2675733
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2024-04-04 03:23:44 -0400

    feat: adding data and parser json to object

---
+++ lib/src/main/java/fuzzy/matching/model/User.java
@@ -0,0 +1,167 @@
+package fuzzy.matching.model;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a user.
+ */
+public class User {
+    
+    @SerializedName("ID")
+    private String id;
+
+    @SerializedName("Name")
+    private String name;
+
+    @SerializedName("Age")
+    private int age;
+
+    @SerializedName("Gender")
+    private String gender;
+
+    private ArrayList<Book> bookList;
+
+    @SerializedName("BookList")
+    private List<String> idBookList;
+
+    /**
+     * Constructs a new User object with the specified id, name, age, gender, and book list.
+     *
+     * @param id       the user's id
+     * @param name     the user's name
+     * @param age      the user's age
+     * @param gender   the user's gender
+     * @param bookList the user's book list
+     */
+    public User(String id, String name, int age, String gender, ArrayList<Book> bookList) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.bookList = bookList;
+        idBookList = new ArrayList<>();
+        bookList = new ArrayList<>();
+    }
+
+    /**
+     * Returns the user's id.
+     *
+     * @return the user's id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the user's id.
+     *
+     * @param id the user's id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the user's name.
+     *
+     * @return the user's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the user's name.
+     *
+     * @param name the user's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the user's age.
+     *
+     * @return the user's age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Sets the user's age.
+     *
+     * @param age the user's age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Returns the user's gender.
+     *
+     * @return the user's gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sets the user's gender.
+     *
+     * @param gender the user's gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * Returns the user's book list.
+     *
+     * @return the user's book list
+     */
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    /**
+     * Sets the user's book list.
+     *
+     * @param bookList the user's book list
+     */
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    /**
+     * Returns the user's book list ids.
+     *
+     * @return the user's book list ids
+     */
+    public List<String> getBookListIds() {
+        return idBookList;
+    }
+
+    /**
+     * Sets the user's book list ids.
+     *
+     * @param idBookList the user's book list ids
+     */
+    public void setBookListIds(ArrayList<String> idBookList) {
+        this.idBookList = idBookList;
+    }
+
+    /**
+     * Adds a book to the user's book list.
+     *
+     * @param book the book to add
+     */
+    public void addBook(Book book) {
+        if (bookList == null) {
+            bookList = new ArrayList<>();
+        }
+        bookList.add(book);
+    }
+}
---
+++ lib/src/main/java/fuzzy/matching/util/Data.java
@@ -0,0 +1,54 @@
+package fuzzy.matching.util;
+import fuzzy.matching.model.Book;
+import fuzzy.matching.model.User;
+import java.util.List;
+
+/**
+ * The Data class provides methods to retrieve books and users from JSON files.
+ */
+public class Data {
+    /**
+     * Retrieves a list of books from a JSON file.
+     *
+     * @return The list of books.
+     */
+    public List<Book> getBooks() {
+        JsonReader<Book> jsonReader = new JsonReader<>();
+        List<Book> books = jsonReader.parseJsonToList("lib/src/main/resources/book.json", Book.class);
+        return books;
+    }
+
+    /**
+     * Retrieves a list of users from a JSON file and populates their book lists.
+     *
+     * @return The list of users.
+     */
+    public List<User> getUsers() {
+        JsonReader<User> jsonReader = new JsonReader<>();
+        List<User> users = jsonReader.parseJsonToList("lib/src/main/resources/users.json", User.class);
+        List<Book> books = getBooks();
+        for(User user : users) {
+            for(String id : user.getBookListIds()) {
+                Book book = getBookById(books, id);
+                user.addBook(book);
+            }
+        }
+        return users;
+    }
+
+    /**
+     * Retrieves a book from a list of books based on its ID.
+     *
+     * @param books The list of books.
+     * @param id The ID of the book to retrieve.
+     * @return The book with the specified ID, or null if not found.
+     */
+    private Book getBookById(List<Book> books, String id) {
+        for (Book book : books) {
+            if (book.getId().equals(id)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
